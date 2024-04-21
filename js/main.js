40f4fbf2d089fd73bf869912a3818224a35ae9ff
Mohammad Rashed <mo2002mohammad@gmail.com>
2024-04-21 14:33:49 -0300

    VICE CITY

--- js/main.js
+++ js/main.js
@@ -52,36 +52,3 @@ let desc = document.querySelector('.desc');
 sliderMain.on('slideChange', e => {
     sliderMain.activeIndex > 0 ? desc.classList.add('hidden') : desc.classList.remove('hidden');
 });
-
-
-// Function called when the YouTube Iframe API is ready
-function onYouTubeIframeAPIReady() {
-    // Create a new YouTube player object
-    const player = new YT.Player('player', {
-        height: '0',
-        width: '0',
-        videoId: 'kTCOQSHohsE', // Video ID of the YouTube video
-        playerVars: {
-            'autoplay': 1, // Autoplay the video
-            'loop': 1, // Loop the video
-            'controls': 0, // Hide player controls
-            'rel': 0, // Hide related videos at the end
-            'showinfo': 0, // Hide video title and uploader info
-            'mute': 1, // Mute the video initially
-            'playlist': 'kTCOQSHohsE' // Required for looping
-        },
-        events: {
-            'onReady': onPlayerReady
-        }
-    });
-
-    // Function called when the player is ready to receive API calls
-    function onPlayerReady(event) {
-        // Set the volume to a low level (value between 0 and 100)
-        event.target.setVolume(10); // Adjust volume as needed (10 is low volume)
-        // Unmute the video after a delay (to allow the volume change to take effect)
-        setTimeout(function () {
-            event.target.unMute();
-        }, 1000);
-    }
-}
